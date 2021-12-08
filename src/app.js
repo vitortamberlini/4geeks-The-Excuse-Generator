dbcacc1752e494038a45718057174e4033a24599
vitortamberlini <vitor.tamberlini@outlook.com>
2021-12-08 18:53:30 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+
+window.onload = function() {
+  //write your code here
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let excuse = `${randomChoice(who)} ${randomChoice(action)} ${randomChoice(
+    what
+  )} ${randomChoice(when)}.`;
+
+  document.getElementById("excuse").innerText = excuse;
+};
+
+function randomNumber(range) {
+  return Math.floor(Math.random() * range);
+}
+
+function randomChoice(array) {
+  return array[randomNumber(array.length)];
+}
